d967739f993ffc912f73258515a1b5a8590230af
mdawsondev <mdawsondev@gmail.com>
2017-08-26 21:45:19 -0400

    Pull arrays from external .txt files

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,8 +1,22 @@
-var animals = ["Dog", "Cat", "Horse", "Wolf", "Salmon", "Hamster", "Badger"];
-var colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
-var food = ["Sandwich", "Pickle", "Peach", "Banana", "Chocolate", "Olive", "Cracker"]
-var numbers = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
-var nature = ["Grass", "Gravel", "Rock", "Swamp", "Lake", "River"];
+var types = ["animals", "colors", "food", "nature", "numbers"];
+
+for (i in types) { 
+	function readTextFile(file) {
+	    var rawFile = new XMLHttpRequest();
+	    rawFile.open("GET", file, false);
+	    rawFile.onreadystatechange = function () {
+	        if (rawFile.readyState === 4) {
+	            if (rawFile.status === 200 || rawFile.status == 0) {
+	                var allText = rawFile.responseText;
+	                alert(types[i]);
+	                types[i] = allText.split('\n'); 
+	            }
+	        }
+	    }
+	    rawFile.send(null);
+	}
+	readTextFile("https://raw.githubusercontent.com/mdawsondev/AliasGenerator/master/data/" + types[i] + ".txt");
+}
 
 window.onload = function() {
   document.getElementById("go").onclick = function () {
@@ -11,15 +25,18 @@ window.onload = function() {
 		var category = [];
 
     if (document.getElementById("animals").checked)
-            category.push(animals);
+            category.push(types[0]);
     if (document.getElementById("colors").checked)
-            category.push(colors);
+            category.push(types[1]);
     if (document.getElementById("food").checked)
-            category.push(food);
+            category.push(types[2]);
     if (document.getElementById("numbers").checked)
-            category.push(numbers);
+            category.push(types[3]);
     if (document.getElementById("nature").checked)
-            category.push(nature);
+            category.push(types[4]);
+
+//pick random category from category
+//pick random item from that category
 
     for (i=0; i<count; i++){
     	var rdmCat = Math.floor(Math.random() * category.length);

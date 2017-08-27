c8aa06d798f77a9b3f67a379012c66fb55e60e49
mdawsondev <mdawsondev@gmail.com>
2017-08-26 21:52:33 -0400

    Convert array push to for loop

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,4 +1,5 @@
 var types = ["animals", "colors", "food", "nature", "numbers"];
+var data = [];
 
 for (i in types) { 
 	function readTextFile(file) {
@@ -8,8 +9,7 @@ for (i in types) {
 	        if (rawFile.readyState === 4) {
 	            if (rawFile.status === 200 || rawFile.status == 0) {
 	                var allText = rawFile.responseText;
-	                alert(types[i]);
-	                types[i] = allText.split('\n'); 
+	                data[i] = allText.split('\n'); 
 	            }
 	        }
 	    }
@@ -24,20 +24,11 @@ window.onload = function() {
 		var alias = "";
 		var category = [];
 
-    if (document.getElementById("animals").checked)
-            category.push(types[0]);
-    if (document.getElementById("colors").checked)
-            category.push(types[1]);
-    if (document.getElementById("food").checked)
-            category.push(types[2]);
-    if (document.getElementById("numbers").checked)
-            category.push(types[3]);
-    if (document.getElementById("nature").checked)
-            category.push(types[4]);
-
-//pick random category from category
-//pick random item from that category
-
+		for (i in types) {
+    	if (document.getElementById(types[i]).checked)
+      	category.push(data[i]);
+    }
+    
     for (i=0; i<count; i++){
     	var rdmCat = Math.floor(Math.random() * category.length);
     	var rdmItem = Math.floor(Math.random() * category[rdmCat].length);

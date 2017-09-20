84176fa060d686eb3cb9c481a0a1e74f5f2a4a0a
mdawsondev <mdawsondev@gmail.com>
2017-09-20 18:37:35 -0400

    Add curls to single statements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -33,8 +33,9 @@ for (var i in categories) {
 //Import external lists and inject them into categories.
 function harvest() {
   for (var i in data) {
-    if (data[i].name === 'custom') //Ignore custom, it's internalized!
+    if (data[i].name === 'custom') { //Ignore custom, it's internalized!
       continue;
+    }
     function readTextFile(file) {
       var rawFile = new XMLHttpRequest();
       rawFile.open('GET', file, false);
@@ -61,7 +62,7 @@ function harvest() {
             document.getElementById('selections').appendChild(wrapper);
           }
         }
-      }
+      };
       rawFile.send(null);
     }
     readTextFile('https://raw.githubusercontent.com/mdawsondev/alias-generator/master/src/data/' + data[i].name + '.txt');
@@ -93,11 +94,13 @@ window.onload = function () {
     }
 
     if (!cOnly.checked) {
-      if (!cPlus.checked)
+      if (!cPlus.checked) {
         custom.content = [];
-      for (i in data){
-        if (document.getElementById(data[i].name).checked)
+      }
+      for (i in data) {
+        if (document.getElementById(data[i].name).checked) {
           library.push(data[i]);
+        }
       }
     }
 
@@ -105,8 +108,9 @@ window.onload = function () {
     //Pick words at random matching our grammar rules.
     var title = false,
     titleLoc = 0;
-    if (library[0].content.length === 0)
+    if (library[0].content.length === 0) {
       library.shift();
+    }
     for (i=0; i<library.length; i++) {
       if (library[i].grammar === 'title') {
         titleLoc = i;
@@ -122,39 +126,46 @@ window.onload = function () {
       rdmThing = library[rdmCat].content[rdmItem];
       //Repeat logic.
       if (norepeats.checked) {
-        if (used.indexOf(rdmThing) < 0)
+        if (used.indexOf(rdmThing) < 0) {
           used.push(rdmThing);
-        else
+        } else {
           continue;
+        }
       }
 
       //Process words before displaying them;
       //Leet speek before caps!
       function goLeet (input) {
         var leetOutput = input;
-        if (input.endsWith('ed'))
+        if (input.endsWith('ed')) {
           leetOutput = input.slice(0, -2) + 'd';
-        if (input.endsWith('er'))
+        }
+        if (input.endsWith('er')) {
           leetOutput = input.slice(0, -2) + 'xor';
-        if (library[rdmCat].grammar === 'verb')
+        }
+        if (library[rdmCat].grammar === 'verb') {
           leetOutput += 'age';
+        }
         leetOutput = leetOutput.replace(/a/g, '4').replace(/e/g, '3').replace(/f/g, 'ph').replace(/i/g, '1').replace(/t/g, '7').replace(/o/g, '0').replace(/s/g, '5').replace(/ate/g, '8');
         return leetOutput;
       }
 
       //Caps logic.
       function goCaps (input) {
-        if (caps.checked)
+        if (caps.checked) {
           input = input.charAt(0).toUpperCase() + input.slice(1);
-        if (capsLock.checked)
+        }
+        if (capsLock.checked) {
           input = input.toUpperCase();
+        }
         if (capsRand.checked) {
           var randOutput = '';
           for (letter in input) {
-            if (Math.floor(Math.random() * 2))
+            if (Math.floor(Math.random() * 2)) {
               randOutput += input[letter].toUpperCase();
-            else
+            } else {
               randOutput += input[letter];
+            }
           }
           input = randOutput;
         }
@@ -163,69 +174,82 @@ window.onload = function () {
 
       //Grammar logic; needs work!
       if (grammar.checked) {
-        if (title && i === 0)
+        if (title && i === 0) {
           rdmThing = library[titleLoc].content[Math.floor(Math.random() * library[titleLoc].content.length)];
+        }
         if (i === count-1 && library[rdmCat].grammar !== 'noun') {
           i -= 1;
           continue;
         }
       }
 
-      if (leet.checked)
+      if (leet.checked) {
         rdmThing = goLeet(rdmThing);
-      if (caps.checked || capsLock.checked || capsRand.checked)
+      }
+      if (caps.checked || capsLock.checked || capsRand.checked) {
         rdmThing = goCaps(rdmThing);
+      }
       alias += rdmThing;
   }
-  if (alias.length == 0)
+  if (alias.length == 0) {
     alias = 'Select a Word Count!'
-  for (i in document.getElementsByClassName('alias0'))
+  }
+  for (i in document.getElementsByClassName('alias0')) {
     document.getElementsByClassName('alias0')[i].innerHTML = alias;
+  }
   document.getElementById('alias1').innerHTML = alias;
   loader.unshift(alias);
-  if (loader.length > 4)
+  if (loader.length > 4) {
     loader.pop();
-  if (loader[1])
+  }
+  if (loader[1]) {
     document.getElementById('alias2').innerHTML = loader[1];
-  if (loader[2])
+  }
+  if (loader[2]) {
     document.getElementById('alias3').innerHTML = loader[2];
-  if (loader[3])
+  }
+  if (loader[3]) {
     document.getElementById('alias4').innerHTML = loader[3];
+  }
 }
 }
 
 function list (el) {
   var icon = el.childNodes[0]
   var elems = document.getElementsByClassName('cat')
-  for (var i = 0 ; i < elems.length; i+=1){
-    if (elems[i].style.display !== 'none')
+  for (var i = 0 ; i < elems.length; i+=1) {
+    if (elems[i].style.display !== 'none') {
       elems[i].style.display = 'none';
-    else
+    } else {
       elems[i].style.display = 'initial';
+    }
   }
-  if ( icon.classList.contains('fa-plus') )
+  if (icon.classList.contains('fa-plus')) {
     icon.className = 'fa fa-minus';
-  else
-    icon.className = 'fa fa-plus';
+  } else {
+    icon.className = 'fa fa-plus'; 
+  }
 }
 
 function swap (id) {
   console.log(id);
   var mute =document.getElementsByClassName('gencontent');
-  for (i=0; i<mute.length; i++){
+  for (i=0; i<mute.length; i++) {
     mute[i].style.display = 'none';
   }
   document.getElementById('gen' + id).style.display = 'flex';
 }
 
 function change(el) {
-  if (el.textContent.includes('Grammar') && document.getElementById('grammar').disabled)
+  if (el.textContent.includes('Grammar') && document.getElementById('grammar').disabled) {
     return;
+  }
   el = el.childNodes[0];
-  if ( el.classList.contains('fa-toggle-off') )
+  if (el.classList.contains('fa-toggle-off')) {
     el.className = 'fa fa-toggle-on';
-  else
+  } else {
     el.className = 'fa fa-toggle-off';
+  }
 }
 
 //Disable options so checkboxes don't conflict.
@@ -244,16 +268,18 @@ function custCheck (id) {
       change(cPlus.nextElementSibling)
       cPlus.checked = false;
     }
-    if (!cOnly.checked)
+    if (!cOnly.checked) {
       grammar.disabled = false;
+    }
     break;
     case 'c-plus':
-    if (cOnly.checked){
+    if (cOnly.checked) {
       change(cOnly.nextElementSibling)
       cOnly.checked = false;
     }
-    if (!cPlus.checked)
+    if (!cPlus.checked) {
       grammar.disabled = false;
+    }
     break;
   }
 }
